a5859a8692f2114df80f4b9443fb76c36ffe2ec8
jerryowusu <jerryowusu50@gmail.com>
2023-09-11 17:30:04 +0000

    Add props

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import Employee from './Components/Employee';
 
 function App() {
+  const showEmployees = true;
+
   return (
     <div className="App">
-       Hello there
+      {showEmployees ? (
+        <div className="employees">
+           <Employee name='Jerry' role= 'Intern'/>
+           <Employee name= 'Wesley'/>
+           <Employee name='Jayceon'/>
+           <Employee name= 'Jarryd' role='Developer'/>
+           <Employee name='Esther' role='Manager'/>
+        </div>
+        )
+        :
+        (
+        <div className="no-employees">
+          Sorry you cannot see the employees
+        </div>
+        )
+      }
+     
+     
+     
+     
+     
     </div>
   );
 }
---
+++ src/Components/Employee.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Employee = (props) => {
+  return (
+    <div className="employees">
+        <h3>Employee: {props.name}</h3>
+        <span>{props.role ? props.role : 'No role yet'}</span>
+    </div>
+  )
+}
+
+export default Employee
