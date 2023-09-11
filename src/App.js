7ac82cea9393d834702e2be7474ed9f7b46d9cb0
jerryowusu <jerryowusu50@gmail.com>
2023-09-11 18:58:43 +0000

    Add tailwind styles

--- src/App.js
+++ src/App.js
@@ -7,18 +7,20 @@ function App() {
   const showEmployees = true;
 
   return (
-    <div className="App bg-blue-500">
+    <div className="App">
       {showEmployees ? (
-        <div className="employees">
-          <input type="text" onChange={(e) => {
+        <div className="employees bg-slate-600">
+          <input className=' bg-blue-400 ' type="text" onChange={(e) => {
             console.log(e.target)
             setRole(e.target.value)
           }}/>
-           <Employee name='Jerry' role= 'Intern'/>
-           <Employee name= 'Wesley'/>
-           <Employee name='Jayceon' role={role} />
-           <Employee name= 'Jarryd' role='Developer'/>
-           <Employee name='Esther' role='Manager'/>
+          <div className='flex flex-wrap justify-center' >
+            <Employee name='Jerry' role= 'Intern' img= 'https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg'/>
+            <Employee name= 'Wesley' img= 'https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg'/>
+            <Employee name='Jayceon' role={role} img= 'https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg'/>
+            <Employee name= 'Jarryd' role='Developer' img= 'https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg'/>
+            <Employee name='Esther' role='Manager' img= 'https://images.pexels.com/photos/3831645/pexels-photo-3831645.jpeg'/>
+          </div>
         </div>
         )
         :
--- src/Components/Employee.jsx
+++ src/Components/Employee.jsx
@@ -3,9 +3,20 @@ import React from 'react'
 const Employee = (props) => {
 
   return (
-    <div className="employees">
-        <h3>Employee: {props.name}</h3>
-        <span>{props.role ? props.role : 'No role yet'}</span>
+
+    <div className="py-8 px-8 m-2 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+        <img className="block mx-auto rounded-full h-[100px] w-[100px] sm:mx-0 sm:shrink-0" src={props.img} alt="Woman's Face" />
+        <div className="text-center space-y-2 sm:text-left">
+            <div className="space-y-0.5">
+            <p className="text-lg text-black font-semibold">
+                {props.name}
+            </p>
+            <p className="text-slate-500 font-medium">
+            {props.role}
+            </p>
+            </div>
+            <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">Message</button>
+        </div>
     </div>
   )
 }
