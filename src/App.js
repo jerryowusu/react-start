aacf04eae55c5d51ce9cd4040492390329164e81
jerryowusu <jerryowusu50@gmail.com>
2023-09-12 15:21:07 +0000

    Update and add new employee functions

--- src/App.js
+++ src/App.js
@@ -1,77 +1,122 @@
 import { useState } from 'react';
 import './App.css';
-import Employee from './Components/Employee';
 import { v4 as uuidv4 } from 'uuid';
+import Employee from './Components/Employee';
+import AddEmployee from './Components/AddEmployee';
+import EditEmployee from './Components/EditEmployee';
+
 
 function App() {
   const [role, setRole] =useState('dev');
   const [employees, setEmployees] =useState([
       {
+        id: 1,
         name: 'Sophia',
         role: 'Developer',
-        img: 'https://media.istockphoto.com/id/1281698360/photo/natural-afro-hair-wide-toothy-smile-and-expression-of-gladness-on-the-face-of-young-brown.jpg'
+        img: 'https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg'
       },
       {
+        id: 2,
         name: 'Sal',
         role: 'Manager',
         img: 'https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg'
       }, 
       {
+        id: 3,
         name: 'John',
         role: 'Director of Eng.',
         img: 'https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg'
       }, 
       {
+        id: 4,
         name: 'Caleb',
         role: 'UI/UX',
         img: 'https://images.pexels.com/photos/6945554/pexels-photo-6945554.jpeg'
       }, 
       {
+        id: 5,
         name: 'Esther',
         role: 'Developer',
         img: 'https://images.pexels.com/photos/8084235/pexels-photo-8084235.jpeg'
       }, 
       {
+        id: 6,
         name: 'Jennifer',
         role: 'Developer',
         img: 'https://images.pexels.com/photos/6945664/pexels-photo-6945664.jpeg'
       }, 
       {
+        id: 7,
         name: 'Lynnete',
         role: 'Backend ',
         img: 'https://images.pexels.com/photos/6405673/pexels-photo-6405673.jpeg'
       },
       {
+        id: 8,
         name: 'Nicey',
         role: 'Developer',
-        img: 'https://media.istockphoto.com/id/969233490/photo/young-african-woman-smiling-at-sunset.jpg'
+        img: 'https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg'
       }
     ]);
   
+  const updateEmployee = (id, newName, newRole, newImg) => {
+    const updatedEmployees = employees.map((employee) => {
+      if(id === employee.id) {
+        return {...employee, name: newName, role: newRole, img: newImg}
+      }
+      return employee;
+    });
+    setEmployees(updatedEmployees);
+  }
+
+  const addNewEmployee = (name, role, img) => {
+     const newEmployee = {
+      id: uuidv4(),
+      name: name,
+      role: role,
+      img: img
+     }
+     setEmployees([...employees, newEmployee])
+  }
+
   const showEmployees = true;
 
 
   return (
     <div className="App">
       {showEmployees ? (
-        <div className="employees bg-slate-600">
-          <input className=' bg-blue-400 ' type="text" onChange={(e) => {
+        <div className="employees">
+
+          <input className='bg-gray-50 border border-p-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 w-full block mx-auto p-2.5 dark:bg-gray-700 dark:border-purple-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 ' type="text" onChange={(e) => {
             console.log(e.target)
             setRole(e.target.value)
           }}/>
+
           <div className='flex flex-wrap justify-center'>
             {employees.map((employee) => {
-              console.log(employee);
+              const editEmployee = (
+                <EditEmployee 
+                  id={employee.id}
+                  name={employee.name} 
+                  role={employee.role} 
+                  img={employee.img}
+                  updateEmployee={updateEmployee}
+                />
+
+              )
               return (
                 <Employee 
-                    key= {uuidv4()}
+                    key= {employee.id}
+                    id={employee.id}
                     name={employee.name} 
                     role={employee.role} 
                     img={employee.img}
+                    editEmployee={editEmployee}
                 />
-              )
-            })}
+                )
+              })}
           </div>
+          <AddEmployee addNewEmployee={addNewEmployee}/>
         </div>
         )
         :
@@ -81,10 +126,7 @@ function App() {
         </div>
         )
       }
-     
-     
-     
-     
+      
      
     </div>
   );
--- src/Components/Employee.jsx
+++ src/Components/Employee.jsx
@@ -4,7 +4,7 @@ const Employee = (props) => {
 
   return (
 
-    <div className="py-8 px-8 m-2 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+    <div className="min-w-[250px] py-8 px-8 m-2 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
         <img className="block mx-auto rounded-full h-[100px] w-[100px] sm:mx-0 sm:shrink-0" src={props.img} alt="Woman's Face" />
         <div className="text-center space-y-2 sm:text-left">
             <div className="space-y-0.5">
@@ -15,9 +15,8 @@ const Employee = (props) => {
             {props.role}
             </p>
             </div>
-            <button className="px-4 py-1 text-sm text-purple-600 font-semibold rounded-full border border-purple-200 hover:text-white hover:bg-purple-600 hover:border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-offset-2">
-                Update
-            </button>
+           
+            {props.editEmployee}
         </div>
     </div>
   )
