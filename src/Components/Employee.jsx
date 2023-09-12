4f3b3ca620c370cab5088607505cf0ed3e209430
jerryowusu <jerryowusu50@gmail.com>
2023-09-12 16:58:27 +0000

    Introduce pages and children props

--- src/Components/Employee.jsx
+++ src/Components/Employee.jsx
@@ -4,7 +4,7 @@ const Employee = (props) => {
 
   return (
 
-    <div className="min-w-[250px] py-8 px-8 m-2 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
+    <div className="min-w-[300px] py-8 px-8 m-2 max-w-sm bg-white rounded-xl shadow-lg space-y-2 sm:py-4 sm:flex sm:items-center sm:space-y-0 sm:space-x-6">
         <img className="block mx-auto rounded-full h-[100px] w-[100px] sm:mx-0 sm:shrink-0" src={props.img} alt="Woman's Face" />
         <div className="text-center space-y-2 sm:text-left">
             <div className="space-y-0.5">
---
+++ src/Pages/Employees.jsx
@@ -0,0 +1,130 @@
+import { useState } from 'react';
+import '../App.css';
+import { v4 as uuidv4 } from 'uuid';
+import Employee from '../Components/Employee';
+import AddEmployee from '../Components/AddEmployee';
+import EditEmployee from '../Components/EditEmployee';
+
+
+function Employees() {
+  const [role, setRole] =useState('dev');
+  const [employees, setEmployees] =useState([
+      {
+        id: 1,
+        name: 'Sophia',
+        role: 'Developer',
+        img: 'https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg'
+      },
+      {
+        id: 2,
+        name: 'Sal',
+        role: 'Manager',
+        img: 'https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg'
+      }, 
+      {
+        id: 3,
+        name: 'John',
+        role: 'Director of Eng.',
+        img: 'https://images.pexels.com/photos/2379004/pexels-photo-2379004.jpeg'
+      }, 
+      {
+        id: 4,
+        name: 'Caleb',
+        role: 'UI/UX',
+        img: 'https://images.pexels.com/photos/6945554/pexels-photo-6945554.jpeg'
+      }, 
+      {
+        id: 5,
+        name: 'Esther',
+        role: 'Developer',
+        img: 'https://images.pexels.com/photos/8084235/pexels-photo-8084235.jpeg'
+      }, 
+      {
+        id: 6,
+        name: 'Jennifer',
+        role: 'Developer',
+        img: 'https://images.pexels.com/photos/6945664/pexels-photo-6945664.jpeg'
+      }, 
+      {
+        id: 7,
+        name: 'Lynnete',
+        role: 'Backend ',
+        img: 'https://images.pexels.com/photos/6405673/pexels-photo-6405673.jpeg'
+      },
+      {
+        id: 8,
+        name: 'Nicey',
+        role: 'Developer',
+        img: 'https://images.pexels.com/photos/1239291/pexels-photo-1239291.jpeg'
+      }
+    ]);
+  
+  const updateEmployee = (id, newName, newRole, newImg) => {
+    const updatedEmployees = employees.map((employee) => {
+      if(id === employee.id) {
+        return {...employee, name: newName, role: newRole, img: newImg}
+      }
+      return employee;
+    });
+    setEmployees(updatedEmployees);
+  }
+
+  const addNewEmployee = (name, role, img) => {
+     const newEmployee = {
+      id: uuidv4(),
+      name: name,
+      role: role,
+      img: img
+     }
+     setEmployees([...employees, newEmployee])
+  }
+
+  const showEmployees = true;
+
+
+  return (
+    <div className="App bg-gray-300 min-h-screen">
+      {showEmployees ? (
+        <div className="employees m-3">
+
+          <div className='flex flex-wrap justify-center'>
+            {employees.map((employee) => {
+              const editEmployee = (
+                <EditEmployee 
+                  id={employee.id}
+                  name={employee.name} 
+                  role={employee.role} 
+                  img={employee.img}
+                  updateEmployee={updateEmployee}
+                />
+
+              )
+              return (
+                <Employee 
+                    key= {employee.id}
+                    id={employee.id}
+                    name={employee.name} 
+                    role={employee.role} 
+                    img={employee.img}
+                    editEmployee={editEmployee}
+                />
+                )
+              })}
+          </div>
+          <AddEmployee addNewEmployee={addNewEmployee}/>
+        </div>
+        )
+        :
+        (
+        <div className="no-employees">
+          Sorry you cannot see the employees
+        </div>
+        )
+      }
+      
+     
+    </div>
+  );
+}
+
+export default Employees;
